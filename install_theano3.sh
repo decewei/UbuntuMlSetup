9d054cb3a50579c1fd216cb193df946fbe32e9e1
Celine <celinew1221@gmail.com>
2018-01-17 16:29:33 -0500

    delete reboot

--- install_theano3.sh
+++ install_theano3.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 THEANO_PATH='/home/celine/.environments/theano'
 USER=`sh get_current_user.sh`
-# pyvenv $THEANO_PATH
-# sudo chown -R $USER:$USER $THEANO_PATH
-# source $THEANO_PATH/bin/activate
-# pip3 install numpy matplotlib keras scikit-image scikit-learn sk-video h5py theano
+pyvenv $THEANO_PATH
+sudo chown -R $USER:$USER $THEANO_PATH
+source $THEANO_PATH/bin/activate
+pip3 install numpy matplotlib keras scikit-image scikit-learn sk-video h5py theano
 
 echo "
 export THEANO_PATH=$THEANO_PATH
--- nvidia_clean_install.sh
+++ nvidia_clean_install.sh
@@ -5,14 +5,12 @@ if [ $# -lt 1 ]; then
     exit 64
 fi
 
-sudo apt-get purge nvidia*
+sudo apt-get purge nvidia-*
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt-get install -y build-essential
-sudo apt-get-repository ppa:graphics-drivers/ppa
+sudo apt-get install build-essential
+sudo apt-add-repository ppa:graphics-drivers/ppa
 sudo apt-get update
-sudo apt-get install -y nvidia-$1
-sudo apt-get install -y mesa-common-dev
-sudo apt-get install -y freeglut3-dev
-read -p "Press [Enter] to reboot"
-sudo reboot
+sudo apt-get install nvidia-$VERSION
+sudo apt-get install mesa-common-dev
+sudo apt-get install freeglut3-dev
