320953337eaa98844762abbbf761684b446daef5
Celine Wei <c.wei@surgicalsafety.com>
2019-03-23 19:47:45 -0400

    new compilation command

--- install_opencv.sh
+++ install_opencv.sh
@@ -2,23 +2,7 @@
 
 USER=`sh get_current_user.sh`
 
-sudo apt-get remove x264 libx264-dev
-sudo apt-get --assume-yes install build-essential checkinstall cmake pkg-config yasm gfortran git
-sudo apt-get --assume-yes install libjpeg8-dev libjasper-dev libpng12-dev
-# If you are using Ubuntu 16.04
-sudo apt-get --assume-yes install libtiff5-dev
-sudo apt-get --assume-yes install libavcodec-dev libavformat-dev libswscale-dev libdc1394-22-dev
-sudo apt-get --assume-yes install libxine2-dev libv4l-dev
-sudo apt-get --assume-yes install libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev
-sudo apt-get --assume-yes install libqt4-dev libgtk2.0-dev libtbb-dev
-sudo apt-get --assume-yes install libatlas-base-dev
-sudo apt-get --assume-yes install libfaac-dev libmp3lame-dev libtheora-dev
-sudo apt-get --assume-yes install libvorbis-dev libxvidcore-dev
-sudo apt-get --assume-yes install libopencore-amrnb-dev libopencore-amrwb-dev
-sudo apt-get --assume-yes install x264 v4l-utils
-sudo apt-get --assume-yes install libprotobuf-dev protobuf-compiler
-sudo apt-get --assume-yes install libgoogle-glog-dev libgflags-dev
-sudo apt-get --assume-yes install libgphoto2-dev libeigen3-dev libhdf5-dev doxygen
+
 
 cd
 git clone https://github.com/opencv/opencv.git
@@ -41,19 +25,30 @@ install ffmpeg from src with --enable_shared
 install gstreamer0.10 gstsreamer1.0
 sudo apt install cmake
 
-cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local \
-      -D WITH_CUDA=ON -D ENABLE_FAST_MATH=1 -D CUDA_FAST_MATH=1 \
-      -D WITH_CUBLAS=ON \
-      -D INSTALL_PYTHON_EXAMPLES=ON \
-      -D ENABLE_PRECOMPILED_HEADERS=OFF \
-      -D BUILD_TEST=OFF \
-      -D CUDA_NVCC_FLAGS="-D_FORCE_INLINES" \
-      -D CUDA_GENERATION=Volta \
-      -D WITH_FFMPEG=ON \
-      -D WITH_GSTREAMER=ON \
-      -D WITH_LIBV4L=ON \
-      -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib/modules \
-      -D WITH_TBB=ON -D WITH_GTK=ON -D WITH_V4L=ON -D WITH_OPENGL=ON -DWITH_QT=OFF ..
+# cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local \
+#       -D WITH_CUDA=ON -D ENABLE_FAST_MATH=1 -D CUDA_FAST_MATH=1 \
+#       -D WITH_CUBLAS=ON \
+#       -D INSTALL_PYTHON_EXAMPLES=ON \
+#       -D ENABLE_PRECOMPILED_HEADERS=OFF \
+#       -D BUILD_TEST=OFF \
+#       -D CUDA_NVCC_FLAGS="-D_FORCE_INLINES" \
+#       -D CUDA_GENERATION=Pascal \
+#       -D WITH_FFMPEG=ON \
+#       -D WITH_GSTREAMER=ON \
+#       -D WITH_LIBV4L=ON \
+#       -D OPENCV_EXTRA_MODULES_PATH=/home/celine/Downloads/opencv_contrib-3.4.5/modules \
+#       -D OPENCV_ENABLE_NONFREE=ON \
+#       -D WITH_TBB=ON -D WITH_GTK=ON -D WITH_V4L=ON -D WITH_OPENGL=ON -DWITH_QT=OFF ..
+
+cmake -D CMAKE_BUILD_TYPE=RELEASE \
+    -D CMAKE_INSTALL_PREFIX=/usr/local \
+    -D WITH_CUDA=ON -D ENABLE_FAST_MATH=1 -D CUDA_FAST_MATH=1 \
+    -D WITH_CUBLAS=ON \
+    -D WITH_FFMPEG=ON \
+    -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib-3.4.5/modules/ \
+    -D OPENCV_ENABLE_NONFREE=ON \
+    -D WITH_EIGEN=ON\
+    -D WITH_TBB=ON -D WITH_GTK=ON -D WITH_V4L=ON -D WITH_OPENGL=ON -DWITH_QT=OFF ..
 
 # # find out number of CPU cores in your machine
 # # substitute 4 by output of nproc
